3b9cc623c7a5b62f790a4003b0df7ae0e1104bbd
Lindsay <lliny0214tw@icloud.com>
2021-07-20 00:00:14 +0800

    comments_apiversion

---
+++ comments__adv/api_comments.php
@@ -0,0 +1,61 @@
+<?php
+	require_once('conn.php');
+	header('Content-type:application/json;charset=utf-8');
+	header('Access-Control-Allow-Origin: *');
+	
+	if( empty($_GET['site_key'])) {
+		$json = array (
+			"ok" => false,
+			"message" => "Please send site_key in url"
+		);
+
+		$response = json_encode($json);
+		echo $response;
+		die();
+	}
+
+	$site_key = $_GET['site_key'];
+
+	$sql = "select id, nickname, content, created_at from lindsay_w12_discussions where site_key = ? " . 
+			(empty($_GET['before']) ? "" : "and id < ?") .
+			" order by id desc limit 2";
+	$stmt = $conn->prepare($sql);
+	if (empty($_GET['before'])){
+		$stmt->bind_param('s', $site_key);
+	} else {
+		$stmt->bind_param('si', $site_key, $_GET['before']);
+	}
+	
+	$result = $stmt->execute();
+
+	if(!$result){
+		$json = array(
+			"ok" => false,  
+			"message" => $conn->error
+		);
+		$response = json_encode($json);
+		echo $response;
+		die();
+	}
+
+	$result = $stmt->get_result();
+	$discussions = array();
+	while($row = $result->fetch_assoc()){
+		array_push($discussions, array(
+			"id" => $row["id"],
+			"nickname" => $row["nickname"],
+			"content" => $row["content"],
+			"created_at" => $row["created_at"],
+		));
+	}
+
+	$json = array(
+		"ok" => true,
+		"discussions" => $discussions
+	);
+
+	$response = json_encode($json);
+	echo $response;
+
+
+?>
